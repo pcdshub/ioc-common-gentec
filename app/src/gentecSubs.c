87ea2cca49afcd807f4de558b90ea8660273816f
Tyler Johnson <tjohnson@slac.stanford.edu>
2017-06-20 21:04:44 +0000

    Adding in signal triggering
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/ioc/common/gentec/current@28425 ececa60f-9f63-0410-97c7-ea05888eb260

--- app/src/gentecSubs.c
+++ app/src/gentecSubs.c
@@ -3,6 +3,7 @@
 #include <aSubRecord.h>
 #include <registryFunction.h>
 #include <string.h>
+#include <stdio.h>
 
 /*
 record(aSub, "RECORDNAME") {
@@ -80,5 +81,83 @@ long gentecNameProc(struct aSubRecord *psub)
     return 0;
 }
 
+/*
+record(aSub, "RECORDNAME") {
+     field(DESC, "Translate Gentec Scale Vals")
+     field(INAM, "gentecScaleInit")
+     field(SNAM, "gentecScaleProc")
+     field(INPA, "LONGIN_RECORD")
+     field(FTA,  "LONG")
+     field(VALA, "AO_RECORD")
+     field(FTVA, "STRING")
+}
+*/
+
+static double gentecScaleValues[] = {
+    0.000000000001,
+    0.000000000003,
+    0.000000000010,
+    0.000000000030,
+    0.000000000100,
+    0.000000000300,
+    0.000000001000,
+    0.000000003000,
+    0.000000010000,
+    0.000000030000,
+    0.000000100000,
+    0.000000300000,
+    0.000001000000,
+    0.000003000000,
+    0.000010000000,
+    0.000030000000,
+    0.000100000000,
+    0.000300000000,
+    0.001000000000,
+    0.003000000000,
+    0.010000000000,
+    0.030000000000,
+    0.100000000000,
+    0.300000000000,
+    1.000000000000,
+    3.000000000000,
+    10.000000000000,
+    30.000000000000,
+    100.000000000000,
+    300.000000000000,
+    1000.000000000000,
+    3000.000000000000,
+    10000.000000000000,
+    30000.000000000000,
+    100000.000000000000,
+    300000.000000000000,
+    1000000.000000000000,
+    3000000.000000000000,
+    10000000.000000000000,
+    30000000.000000000000,
+    100000000.000000000000,
+    300000000.000000000000,
+};
+
+long gentecValInit(struct aSubRecord *psub)
+{
+    psub->dpvt = 0;
+    return 0;
+}
+
+long gentecValProc(struct aSubRecord *psub)
+{
+    epicsInt32 id  = *((epicsInt32 *) (psub->a));
+    double *value     = ((double *) (psub->vala));
+
+    if (id < 0 || id >= sizeof(gentecScaleValues) / sizeof(double))
+    *value = -1;
+    else
+    *value = gentecScaleValues[id];
+    printf("%d --> %f\n", id, *value);
+    return 0;
+}
+
 epicsRegisterFunction(gentecNameInit);
 epicsRegisterFunction(gentecNameProc);
+epicsRegisterFunction(gentecValInit);
+epicsRegisterFunction(gentecValProc);
